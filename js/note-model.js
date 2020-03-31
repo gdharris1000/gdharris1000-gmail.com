aef1f3f4647026f2b18b2a998faf60a6187e4d78
Gareth Harris <gdharris1000@gmail.com>
2020-03-31 09:37:55 +0100

    refactored note-model with module pattern

--- js/note-model.js
+++ js/note-model.js
@@ -2,6 +2,17 @@ function Note(text){
     this.text = text;
 };
 
-Note.prototype.display = function(){
-  return this.text;
-}
+Note.prototype = (function(){
+
+  function display(){
+    return this.text;
+  }
+
+  return{
+    display: display
+  }
+
+})()
+// Note.prototype.display = function(){
+//   return this.text;
+// }
